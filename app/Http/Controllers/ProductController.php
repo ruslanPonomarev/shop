e05e3eb1cd1bb35564d2d0ec5edc13f177e68027
Ruslan Ponomarev <ruslan.ponomarev@wearebrain.com>
2020-07-12 12:48:08 +0300

    linkPublicStorage

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -41,12 +41,14 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
+        $path = $request->file('photo')->store('photos', 'public');
+
         Product::create([
             'name' => $request->name,
             'description' => $request->description,
             'price' => $request->price,
             'category_id' => $request->category_id,
-            'photo' => ''
+            'photo' => $path
         ]);
 
         return redirect()->route('products.index');
@@ -106,6 +108,6 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+
     }
 }
